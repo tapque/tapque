8869d1495243cc52c2cbfb14c5ab58bd11758cca
Balamurugan R <bala-1030@bala-1030.csez.zohocorpin.com>
2013-11-16 22:50:12 +0530

    tapque version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tapque::Application.config.secret_key_base = '1d67af3f6a46db1a665227f75f295fcc2670af76ff215ffa50eeee10903ca89a8faaf36bc02894a657cc464d3ba13f17b56711e5a1f83a38b341f6ee5edb423c'
